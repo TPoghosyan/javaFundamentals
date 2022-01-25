9875edc8afa3d55a33ce23cef327a0e27177b745
Tigran Poghosyan <ttiikkooooo@gmail.com>
2022-01-25 16:53:04 +0400

    create class solution ReservOrder class

--- src/main/java/homeWork/ReservOrder.java
+++ src/main/java/homeWork/ReservOrder.java
@@ -1,4 +1,24 @@
 package homeWork;
 
+import java.util.Scanner;
+
 public class ReservOrder {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int inputDataCount = 3;
+        String inputData[] = new String[inputDataCount];
+        for (int i = 0; i < inputDataCount; i++) {
+            System.out.println("Please type your data here");
+            inputData[i] = scanner.nextLine();
+
+        }
+
+        for (int i = 0; i < inputDataCount; i++) {
+            System.out.print((inputData[inputDataCount-i-1]) + "  ");
+        }
+
+
+    }
 }
