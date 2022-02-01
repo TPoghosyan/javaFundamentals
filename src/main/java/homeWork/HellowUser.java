108c1cb0b08989ad1095455916c393b959f1a9fd
Tigran Poghosyan <ttiikkooooo@gmail.com>
2022-02-01 10:55:50 +0400

    refactoring HellowUser,add Uppercase chesk method

--- src/main/java/homeWork/HellowUser.java
+++ src/main/java/homeWork/HellowUser.java
@@ -10,8 +10,13 @@ public class HellowUser {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please type your name here     \"");
         userName = scanner.nextLine();
-        while (checkDigits(userName)) {
-            System.out.print("type true name \"");
+        boolean x = checkFirstUpper(userName);
+        while (checkDigits(userName) || !checkFirstUpper(userName)) {
+            if (checkDigits(userName)) {
+                System.out.println("name may note contain digits ");
+            }else if (!checkFirstUpper(userName)) {
+                System.out.print("the name should start with Uppercase");
+            }
             userName = scanner.nextLine();
         }
         System.out.println("hellow dear  " + userName);
@@ -20,13 +25,23 @@ public class HellowUser {
     static boolean checkDigits(String sample) {
         char[] chars = sample.toCharArray();
         StringBuilder sb = new StringBuilder();
-        boolean x = false;
+        boolean digits = false;
         for (char c : chars) {
             if (Character.isDigit(c)) {
-                x = true;
+                digits = true;
             }
         }
-        return x;
+        return digits;
+    }
+
+    static boolean checkFirstUpper(String sample) {
+        char[] chars = sample.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        boolean upperCase = false;
+        if (Character.isUpperCase(chars[0])) {
+            upperCase = true;
+        }
+        return upperCase;
     }
 
 }
